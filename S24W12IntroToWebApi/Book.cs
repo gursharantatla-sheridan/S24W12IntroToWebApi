bdd2cfd305dbc47d033a172eff368365efa78620
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-08-01 08:29:37 -0400

    Add project files.

---
+++ S24W12IntroToWebApi/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace S24W12IntroToWebApi
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string? Title { get; set; }
+        public double? Price { get; set; }
+    }
+}
