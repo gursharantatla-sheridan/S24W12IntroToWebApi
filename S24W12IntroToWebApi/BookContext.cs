7b5d97f1419cb70c19b4fca77de2671234e33b0e
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-08-01 08:40:25 -0400

    book context

---
+++ S24W12IntroToWebApi/BookContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace S24W12IntroToWebApi
+{
+    public class BookContext : DbContext
+    {
+        // entity set
+        // DbSet property
+        public DbSet<Book> Books { get; set; }
+
+        // constructor
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+        }
+
+        // data seeding
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book { BookId = 1, Title = "HTML", Price = 10 },
+                new Book { BookId = 2, Title = "ASP.NET", Price = 20 }
+            );
+        }
+    }
+}
